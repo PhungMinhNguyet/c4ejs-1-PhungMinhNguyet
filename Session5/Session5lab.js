52a7347c7df9ad9e7c25cda7a3dc873c576b554f
PhungMinhNguyet <hnts.phungminnguyet@gmail.com>
2019-10-15 06:45:41 +0700

    catchInputUppercase

--- Session5/Session5lab.js
+++ Session5/Session5lab.js
@@ -1,99 +1,99 @@
-        ////PART1
+        // ////PART1
 
-        // 1 
-        for (let i = 0; i <= 2; i++) {
-            let randomNumber = Math.random().toFixed(2);
-            console.log(randomNumber);
-        }
+        // // 1 
+        // for (let i = 0; i <= 2; i++) {
+        //     let randomNumber = Math.random().toFixed(2);
+        //     console.log(randomNumber);
+        // }
 
-        // //2
-        let arr = [2, 5, 6, 9, 10];
+        // // //2
+        // let arr = [2, 5, 6, 9, 10];
 
-        let randomIndexInArray = Math.floor(Math.random() * arr.length);
-        console.log(randomIndexInArray);
+        // let randomIndexInArray = Math.floor(Math.random() * arr.length);
+        // console.log(randomIndexInArray);
 
-        console.log("random arr: ", arr[randomIndexInArray]);
+        // console.log("random arr: ", arr[randomIndexInArray]);
 
-        //3-4-5-6-7-8
-        let listQuizz = [{
-                question: 'How many legs does a spider have ?',
-                choice: [`1. None\n`,
+        // //3-4-5-6-7-8
+        // let listQuizz = [{
+        //         question: 'How many legs does a spider have ?',
+        //         choice: [`1. None\n`,
 
-                    `2. 2 \n`,
+        //             `2. 2 \n`,
 
-                    ` 3. 8 \n`,
+        //             ` 3. 8 \n`,
 
-                    `4. 12 \n`
-                ],
-                answer: 1
-            },
+        //             `4. 12 \n`
+        //         ],
+        //         answer: 1
+        //     },
 
-            {
-                question: 'How many eyes does a spider have ?',
-                choice: [`1. None\n`,
+        //     {
+        //         question: 'How many eyes does a spider have ?',
+        //         choice: [`1. None\n`,
 
-                    `2. 2 \n`,
+        //             `2. 2 \n`,
 
-                    ` 3. 8 \n`,
+        //             ` 3. 8 \n`,
 
-                    `4. 12 \n`
-                ],
-                answer: 2
-            },
+        //             `4. 12 \n`
+        //         ],
+        //         answer: 2
+        //     },
 
-            {
-                question: 'How many hands does a spider have ?',
-                choice: [`1. None\n`,
+        //     {
+        //         question: 'How many hands does a spider have ?',
+        //         choice: [`1. None\n`,
 
-                    `2. 2 \n`,
+        //             `2. 2 \n`,
 
-                    ` 3. 8 \n`,
+        //             ` 3. 8 \n`,
 
-                    `4. 12 \n`
-                ],
-                answer: 1
-            }
-
-
-        ];
+        //             `4. 12 \n`
+        //         ],
+        //         answer: 1
+        //     }
 
 
+        // ];
 
 
-        let sum = 0;
-        while (listQuizz.length > 0) {
-            let randomIndexInList = Math.floor(Math.random() * listQuizz.length);
 
-            let re = Number(prompt(`${listQuizz[randomIndexInList].question} \n ${listQuizz[randomIndexInList].choice}`));
-            if (re == listQuizz[randomIndexInList].answer) {
-                alert('bravo');
-                listQuizz.splice(randomIndexInList, 1);
-                sum += 1;
-            } else {
-                alert(" good luck next time ^^");
-                listQuizz.splice(randomIndexInList, 1);
-            }
 
-        }
-        alert(' You are out of ques ');
-        alert(` your correct answer is ${sum} of 3`);
-        //____________________________________________________________________________
+        // let sum = 0;
+        // while (listQuizz.length > 0) {
+        //     let randomIndexInList = Math.floor(Math.random() * listQuizz.length);
 
-        //PART2
+        //     let re = Number(prompt(`${listQuizz[randomIndexInList].question} \n ${listQuizz[randomIndexInList].choice}`));
+        //     if (re == listQuizz[randomIndexInList].answer) {
+        //         alert('bravo');
+        //         listQuizz.splice(randomIndexInList, 1);
+        //         sum += 1;
+        //     } else {
+        //         alert(" good luck next time ^^");
+        //         listQuizz.splice(randomIndexInList, 1);
+        //     }
 
-        //1
-        let listOfWords = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life',
-            'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team'
-        ];
-        for (let i = 0; i < listOfWords.length; i++) {
-            let sum = 1;
-            for (let j = i + 1; j < listOfWords.length; j++) {
-                if (listOfWords[i] == listOfWords[j]) {
-                    sum += 1;
-                }
-            }
-            console.log(` ${listOfWords[i]}:${sum}\n`);
-        }
+        // }
+        // alert(' You are out of ques ');
+        // alert(` your correct answer is ${sum} of 3`);
+        // //____________________________________________________________________________
+
+        // //PART2
+
+        // //1
+        // let listOfWords = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life',
+        //     'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team'
+        // ];
+        // for (let i = 0; i < listOfWords.length; i++) {
+        //     let sum = 1;
+        //     for (let j = i + 1; j < listOfWords.length; j++) {
+        //         if (listOfWords[i] == listOfWords[j]) {
+        //             sum += 1;
+        //         }
+        //     }
+        //     console.log(` ${listOfWords[i]}:${sum}\n`);
+        // }
 
         //2-3-4 
         const inventoryByBrand = {
@@ -144,8 +144,8 @@
 
         let list = [];
         let total = 0;
-        let inputBrand = prompt('Which brand:');
-
+        let inputBran = prompt('Which brand:');
+        let inputBrand = inputBran.toLowerCase();
 
         for (let i = 0; i < inventoryByBrand[inputBrand].length; i++) {
             list.push(inventoryByBrand[inputBrand][i].name);
@@ -166,7 +166,7 @@
                 h += ".";
             }
         }
-
+        inputBrand.toUpperCase();
         alert(`There are ${inventoryByBrand[inputBrand].length} generators of ' ${inputBrand}' in inventory:
             ${stringList}\nwith total value: ${h}VND
              `);
